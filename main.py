9fe639fdb0156ca36288118003e73d820062288d
zzzz465 <zzzz465@naver.com>
2019-01-26 15:28:16 +0900

    출력물을 이쁘게 정리

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pickle
 import Parse
 from time import sleep
 import downloader
+
 #백업라인
 #------------------------------------------------
 temp = os.environ['userprofile']
@@ -30,52 +31,52 @@ mod_list_workshop = [] # 모드 리스트(이름만)
 mod_dic_num = {} #이름 : 번호
 mod_nlist = []
 
-Parse.Parser(mod_dic,mod_dic_num, mod_list_workshop)
+rim64win_path = Parse.Parser(mod_dic,mod_dic_num, mod_list_workshop) # 파싱 작업을 수행 후, 림월드 실행을 위한 파일 경로를 return
 print('현재 구독중인 모드 리스트를 불러옵니다...')
 
 for x in mod_list_workshop:
     sleep(0.1)
     print(x)
 
-config_num = []
 config_num = Parse.mod_sort() # config 리스트에 현재 로딩중인 모드를 리스트로 저장
 print('모드 세팅에서 로딩한 모드를 재배열 합니다...')
 sleep(1)
-Parse.Parser(mod_dic, mod_dic_num, mod_list_workshop)
 
-mod_list_sorted = []
-for mod in config_num:
-    if mod in mod_dic:
+mod_list_sorted = list()
+for mod in config_num: #mod는 숫자
+    print(mod)
+    if mod == 'Core':
+        continue
+    try:
         modname = mod_dic[mod] #mod는 숫자
         mod_list_sorted = mod_list_sorted + [[data[modname], mod]]
-
-    else:
-        if mod == 'Core':
-            print('Core')
-
-        else:
-            modname = mod_dic[mod]
-            mod_nlist.append(modname)
-
-        
+    except:
+        mod_nlist.append(mod)
+        print(mod_dic[mod],' 모드는 template에 없어 제외되었습니다.')
+    print('\n')    
 
 mod_list_sorted.sort()
 
 Parse.setconfig(mod_list_sorted)
 print('배열한 모드 순서는 다음과 같습니다. \n')
-for i in config_num:
+for i in mod_list_sorted:
     sleep(0.1)
-    if i.isdigit():
-        print(mod_dic[i])
+    if i[1].isdigit():
+        print(mod_dic[i[1]])
     else:
         print(i)
 
-if mod_nlist == True:
+if len(mod_nlist) != 0:
     print('다음 모드는 로드 목록에 있었으나, template에 없어서 로드가 해제된 모드입니다. 인-게임에서 수동으로 모드를 배열해주세요.')
     for x in mod_nlist:
-        print(x)
+        print(mod_dic[x])
 
 
 print('배열이 끝났습니다.')
+
+print('림월드를 실행합니다.')
+os.startfile(rim64win_path)
+
+
 
 

ade12a2d74a2b23aac2896fdbac6b0bc34b74d4c
zzzz465 <zzzz465@naver.com>
2019-01-26 17:01:29 +0900

    자동으로 닫히도록 수정

--- downloader.py
+++ downloader.py
@@ -5,27 +5,25 @@ import json
 import shutil
 import tempfile
 def update():
-    tempdir = tempfile.mkdtemp()
-    os.chdir(tempdir)
-    url = 'https://github.com/zzzz465/Rimworld-automatic-mod-sorter/archive/master.zip'
-    with urlopen(url) as res:
-        res_data = res.read()
+    with tempfile.mkdtemp() as tempdir:
+        os.chdir(tempdir)
+        url = 'https://github.com/zzzz465/Rimworld-automatic-mod-sorter/archive/master.zip'
+        with urlopen(url) as res:
+            res_data = res.read()
 
-        with open('./template.zip', 'wb') as f:
-            f.write(res_data)
+            with open('./template.zip', 'wb') as f:
+                f.write(res_data)
 
-    template = zipfile.ZipFile('template.zip')
-    template.extractall('./')
-    template.close()
+        template = zipfile.ZipFile('template.zip')
+        template.extractall('./')
+        template.close()
 
-    os.chdir('./Rimworld-automatic-mod-sorter-master')
+        os.chdir('./Rimworld-automatic-mod-sorter-master')
 
-    f = open('db_template.json', 'r')
-    return_dict = json.loads(f.read())
-    f.close()
-    shutil.rmtree(tempdir)
-    
-    #print(return_dict)
+        f = open('db_template.json', 'r')
+        return_dict = json.loads(f.read())
+        f.close()        
+        #print(return_dict)
     return return_dict #return_dict로 다운받은 최신 dictionary를 받아옴
 
 

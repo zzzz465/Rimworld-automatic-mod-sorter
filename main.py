3ca101b0c89456435baa629ba017544ab70c1057
zzzz465 <zzzz465@naver.com>
2019-01-27 14:52:56 +0900

    모드 저장 구분 local/workshop/__localcopy를 구분하도록 하였습니다.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from time import sleep
 import downloader
 import time
 import sys
+import re
 
 Version = 0.2
 print('현재 버전은 v' + str(Version) + '입니다.')
@@ -43,12 +44,10 @@ if Version < lastest_Version:
 
 sleep(2)
 
-mod_dic = {} # 모드와 번호 연결, 번호 : 이름
-mod_list_workshop = [] # 모드 리스트(이름만)
-mod_dic_num = {} #이름 : 번호
-mod_nlist = []
+mod_dic, mod_dic_num = dict() #mod_dic 번호:이름 // mod_dic_num 이름:번호
+mod_list_workshop, mod_list_local, mod_nlist = list() # 모드 리스트(이름만) // 워크샵 / 로컬 / 로드 리스트에 있었으나 미입력
 
-rim64win_path = Parse.Parser(mod_dic,mod_dic_num, mod_list_workshop) # 파싱 작업을 수행 후, 림월드 실행을 위한 파일 경로를 return
+rim64win_path = Parse.Parser(mod_dic,mod_dic_num, mod_list_workshop,mod_list_local, data) # 파싱 작업을 수행 후, 림월드 실행을 위한 파일 경로를 return
 print('현재 구독중인 모드 리스트를 불러옵니다...')
 print('checking available workshop mods...')
 
@@ -56,22 +55,41 @@ for x in mod_list_workshop:
     sleep(0.1)
     print(x)
 
-config_num = Parse.mod_sort() # config 리스트에 현재 로딩중인 모드를 리스트로 저장
+config_num = Parse.find_activate_mod() # config 리스트에 현재 로딩중인 모드를 리스트로 저장
 print('모드 세팅에서 로딩한 모드를 재배열 합니다...')
 print('sorting mods....')
 sleep(1)
 
 mod_list_sorted = list()
-for mod in config_num: #mod는 숫자
-    if mod == 'Core':
-        continue
+for mod in config_num: #mod는 숫자, 영문이름, 또는 __Localcopy
+    m = re.match('__LocalCopy', mod)
     try:
-        modname = mod_dic[mod] #mod는 숫자
-        mod_list_sorted = mod_list_sorted + [[data[modname], mod]]
+        if m: #만약 config파일에서 불러온 모드의 이름이 __LocalCopy로 시작하면
+            modname = mod.split('_')[3]
+            mod_list_sorted = mod_list_sorted + [[data[modname], mod, True]]
+            print('localcopy 모드를 리스트에 추가')
+            continue
+
+
+        elif mod.isdigit() == False: #Local인데 __Localcopy가 아니라면
+            modname = mod
+            mod_list_sorted = mod_list_sorted + [[data[modname], mod, True]]
+            print('local 모드를 리스트에 추가')
+            continue
+
+        elif mod.isdigit():
+            modname = mod_dic[mod] #mod는 숫자
+            mod_list_sorted = mod_list_sorted + [[data[modname], mod, False]]
+            print('workshop 모드를 리스트에 추가')
+
+
     except:
-        mod_nlist.append(mod)
-        print(mod_dic[mod],' 모드는 template에 없어 제외되었습니다.')
-        print(mod_dic[mod],' this mod is not supported, template need update.')
+        print(modname, ' 은 template에 없어 제외되었습니다.')
+        print(modname, "no data in database, sorry!")
+        mod_nlist.append(modname)
+
+
+    finally:
         print('\n')
         sleep(0.2)    
 
@@ -106,7 +124,15 @@ print('launching Rimworld...')
 os.startfile(rim64win_path)
 
 print('로그를 확인해주세요.')
-sleep(1)
+print('please check full log')
+exit = False
+while exit == False:
+    x = input('type "exit" to exit program : ')
+    x.lower()
+    if x == 'exit':
+        exit = True
+    else:
+        pass
 
 
 

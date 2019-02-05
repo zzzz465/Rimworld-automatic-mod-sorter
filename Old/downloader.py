746f23f5145174c10ffb43fd292080cfdda0ea23
zzzz465 <46273764+zzzz465@users.noreply.github.com>
2019-02-06 08:59:03 +0900

    v0.4 update

---
+++ Old/downloader.py
@@ -0,0 +1,35 @@
+import json
+import os
+import shutil
+import tempfile
+import zipfile
+from urllib.request import urlopen
+
+
+def update():
+    tempdir = tempfile.mkdtemp()
+    os.chdir(tempdir)
+    url = 'https://github.com/zzzz465/Rimworld-automatic-mod-sorter/archive/master.zip'
+    with urlopen(url) as res:
+        res_data = res.read()
+
+        with open('./template.zip', 'wb') as f:
+            f.write(res_data)
+
+    template = zipfile.ZipFile('template.zip')
+    template.extractall('./')
+    template.close()
+
+    os.chdir('./Rimworld-automatic-mod-sorter-master')
+
+    f = open('db_template.json', 'r', encoding='UTF-8')
+    return_dict = json.loads(f.read())
+    f.close()
+    
+    #print(return_dict)
+    return return_dict #return_dict로 다운받은 최신 dictionary를 받아옴
+
+
+
+if __name__ == '__main__':
+    pass
--- downloader.py
+++ downloader.py
@@ -5,31 +5,32 @@ import tempfile
 import zipfile
 from urllib.request import urlopen
 
+DB_url = 'https://raw.githubusercontent.com/zzzz465/Rimworld-automatic-mod-sorter/master/db_template.json'
 
-def update():
+def download_DB(DB, Ver): # DB 다운받아서, DB 반환, 버전 체크도 함께
     tempdir = tempfile.mkdtemp()
     os.chdir(tempdir)
-    url = 'https://github.com/zzzz465/Rimworld-automatic-mod-sorter/archive/master.zip'
-    with urlopen(url) as res:
+    with urlopen(DB_url) as res:
         res_data = res.read()
 
-        with open('./template.zip', 'wb') as f:
+        with open('./DB_template.json', 'wb') as f:
             f.write(res_data)
-
-    template = zipfile.ZipFile('template.zip')
-    template.extractall('./')
-    template.close()
-
-    os.chdir('./Rimworld-automatic-mod-sorter-master')
-
-    f = open('db_template.json', 'r', encoding='UTF-8')
-    return_dict = json.loads(f.read())
-    f.close()
+ 
+    with open('db_template.json', 'r', encoding='UTF-8') as f:
+        DB.update(json.loads(f.read()))
     
-    #print(return_dict)
-    return return_dict #return_dict로 다운받은 최신 dictionary를 받아옴
+    
+    if Ver < DB['Version'] :
+        print('New version detected. please download newer version in github!')
+        
+    else:
+        pass
 
+    return DB
 
 
 if __name__ == '__main__':
-    pass
+    DB = dict()
+    Ver = 1.0
+    download_DB(DB,Ver)
+    print(DB) # 테스트

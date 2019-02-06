d0686e4f5da56a126724772c687e66589b6e44ae
zzzz465 <46273764+zzzz465@users.noreply.github.com>
2019-02-06 22:00:21 +0900

    root.destory 추가, 시작 폴더를 C:에서 프로그램 위치로 변경

--- finder.py
+++ finder.py
@@ -6,9 +6,10 @@ from tkinter import filedialog
 def finder(): # exe 파일, 폴더 찾아서 넘김
     print('please select your rimworld .exe file.')
     print('pop up will open in 2 seconds...')
+    cdir = os.path.dirname(os.path.realpath(__file__))
     sleep(2)
     root = tk.Tk()
-    rimdir = filedialog.askopenfilename(initialdir = 'C:/', title = 'Select rimworldwin64.exe', filetype = [('Rimworld.exe file', '*.exe')])
+    rimdir = filedialog.askopenfilename(initialdir = cdir, title = 'Select rimworldwin64.exe', filetype = [('Rimworld.exe file', '*.exe')])
     root.destroy()
 
     local_mod_folder = '{}/Mods'.format(os.path.dirname(rimdir))
@@ -20,9 +21,11 @@ def finder_folder(): # 폴더 디렉토리 반환
     sleep(1.5)
     root = tk.Tk()
     dir = filedialog.askdirectory(initialdir = 'C:/', title = 'select folder')
+    root.destroy()
 
     return dir
 
 
 if __name__ == '__main__':
-    pass
+    dir = finder_folder()
+    print(dir)

a8d32010b67b7a48d6631701f7460cf084c29612
zzzz465 <zzzz465@naver.com>
2019-03-19 12:43:53 +0900

    fix some code

--- upload.py
+++ upload.py
@@ -4,11 +4,8 @@ import datetime
 import time
 import logging
 
-log = logging.getLogger('gistlog')
-stream_handler = logging.StreamHandler()
-log.addHandler(stream_handler)
 
-def gitupload(str1):
+def gitupload(str1, token):
     '''
     HTTP Reuests has following parameters: 
     1)Request URL 
@@ -23,11 +20,11 @@ def gitupload(str1):
     import json
 
     GITHUB_API="https://api.github.com"
-    API_TOKEN='9e5756b2adfd50bb64583156eee83198bf731cd4'
+    API_TOKEN=str(token)
 
     #form a request URL
     url=GITHUB_API+"/gists"
-    log.info("Request URL: %s"%url)
+    print("Request URL: %s"%url)
 
     #print headers,parameters,payload
     headers={'Authorization':'token %s'%API_TOKEN}
@@ -39,7 +36,7 @@ def gitupload(str1):
 
     #print response --> JSON
     #token = 
-    log.info('status code : {}'.format(res.status_code))
+    print('status code : {}'.format(res.status_code))
     j=json.loads(res.text)
 
     # Print created GIST's details
@@ -53,6 +50,7 @@ if __name__ == '__main__':
     sdfsdf
     Modname3
     modmane4
-    asdf
+    asdfffdsfsfsfsfd
     """
-    gitupload(test_text)
+    print('initializing')
+    #gitupload(test_text)

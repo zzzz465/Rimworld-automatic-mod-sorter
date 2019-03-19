d8d1ef29eb8473be4c9b46457eb3a6dbb69379d0
zzzz465 <zzzz465@naver.com>
2019-03-19 13:18:12 +0900

    replace DB download method

--- downloader.py
+++ downloader.py
@@ -10,36 +10,23 @@ import logging
 
 DB_url = 'https://raw.githubusercontent.com/zzzz465/Rimworld-automatic-mod-sorter/master/db_template.json'
 
-DB = dict()
+log = logging.getLogger('RAMS.downloader')
 
-def download_DB(Ver): # DB 다운받아서, DB 반환, 버전 체크도 함께
-    global DB
-    tempdir = tempfile.mkdtemp()
-    os.chdir(tempdir)
-    with urlopen(DB_url) as res:
-        res_data = res.read()
+def download_DB(): #shakeyourbunny's code. thank you!
+    '''Return latest DB file type <dict>
+    '''
+    log.info('fetching latest DB file from server...')
+    log.info('please allow firewall internet connection.')
+    sleep(1)
+    with urlopen(DB_url) as jsonurl:
+        dbrawdata = jsonurl.read()
+    return json.loads(dbrawdata)
 
-        with open('./DB_template.json', 'wb') as f:
-            f.write(res_data)
- 
-    with open('db_template.json', 'r', encoding='UTF-8') as f:
-        DB.update(json.loads(f.read()))
-    
-    
-    if Ver < DB['Version'] :
-        logging.info('New version detected. please download newer version in github!')
-        logging.info('\n Program will be closed in 5 seconds...')
-        sleep(3)
-        sys.exit(0)
-        
-    else:
-        pass
-
-    return DB
-
+    #with open('db_template.json', 'wb') as dbfile:
+    #    dbfile.write(dbrawdata)
 
 if __name__ == '__main__':
-    DB = dict()
-    Ver = 1.0
-    download_DB(Ver)
-    print(DB) # 테스트
+    DBtest = download_DB()
+    for x in DBtest:
+        print(x)
+    print(type(DBtest))

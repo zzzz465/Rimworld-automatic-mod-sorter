0cdb3ab07d8b880e9c670422a34a2bc598c8078f
zzzz465 <zzzz465@naver.com>
2019-03-30 21:15:16 +0900

    version up

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from core import downloader, RWmanager, upload
 from core.Modmanager import Modmanager
 
 currentdir = os.getcwd()
-Version = 0.54 #dev
+Version = 0.56 #dev
 
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] : %(message)s',"%H:%M:%S") #TODO change to more readable format.
 log = logging.getLogger("RAMS")
